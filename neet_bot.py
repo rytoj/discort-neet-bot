15541bcd8252446196d01c8d4c59649d15a94598
rytoj <you@example.com>
2017-09-01 00:05:49 +0300

    Fix config parsing

--- neet_bot.py
+++ neet_bot.py
@@ -1,50 +1,11 @@
 # -*- coding: utf-8 -*-
-import logging
-import os
-import sys
 import urllib.request
 
-import configparser
 import discord
 from bs4 import BeautifulSoup
+from neet_bot import log_parser
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s %(levelname)-8s %(message)s',
-                    datefmt='%Y-%m-%d %a %H:%M:%S')
-LOGGER = logging.getLogger(__name__)
-
-
-def get_current_path(filename):
-	"""
-	Gets current path.
-	:return: current path
-	"""
-	dir_name = os.path.dirname(sys.argv[0])
-	config = dir_name + '/' + filename
-	if not os.path.isfile(config):
-		LOGGER.info("Trying different path for '%s'", filename)
-		dir_name = os.path.join(os.getcwd())
-		config = dir_name + '/' + filename
-		LOGGER.info(dir_name)
-	return config
-
-
-def get_api_from_config(file_, header, parameter):
-	"""
-	Gets api from config
-	:param file_: file to check path
-	:param header: header from config file
-	:param parameter: parameter to find
-	:return: api from config
-	"""
-	fullpath = get_current_path(file_)
-	dir_name = os.path.dirname(fullpath)
-	parser = configparser.SafeConfigParser()
-	parser.read(dir_name + "/" + 'api.conf')
-	return parser.get(header, parameter)
-
-
-TOKEN = get_api_from_config("settings.conf", "Discord", "BOT_TOKEN")
+TOKEN = log_parser.get_api_from_config("settings.conf", "Discord", "BOT_TOKEN")
 
 
 def make_soup(url):
